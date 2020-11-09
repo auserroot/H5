7a0564ae17c5098d01cb5c08e3fd530dfbbbe406
auserroo <fauserfzy@gmail.com>
2020-11-09 10:47:29 +0800

    Update getdevice_width.js

--- js/getdevice_width.js
+++ js/getdevice_width.js
@@ -1,1 +1,1 @@
-document.documentElement.style.fontSize = document.documentElement.clientWidth  /  3.2 + 'px';//设置页面样式字体大小 =（获取设备宽度（设备为iphone5屛宽320px） / 3.2 ）px
+document.documentElement.style.fontSize = document.documentElement.clientWidth  /  3.2 + 'px';//设置页面样式字体大小（为100px） =（获取设备宽度（设备为iphone5屛宽320px） / 3.2 ）px
